ee537827ae25dc1ab9f4d9818507ef40b041e8bb
Peter Storm <peter.storm@peterstorm.io>
2020-08-30 21:20:03 +0200

    add actual configuration files

--- vim.nix
+++ vim.nix
@@ -1,1 +1,10 @@
-/etc/nixos/vim.nix
+with import <nixpkgs> {};
+
+neovim.override {
+  configure = {
+    customRC = builtins.readFile "/home/peterstorm/.config/nvim/init.vim";
+    packages.myVimPackage = with pkgs.vimPlugins; {
+      start = [ vim-plug ];
+    };
+  };
+}
